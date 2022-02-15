759f1fcfb71cca35bbf0bcd847d8cfd0250cd8d0
Edith-Heidmann <eheidmann@gmail.com>
2022-02-15 19:03:50 +0100

    Added React-Router-Dom 6 and setup folder structure for components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
 import styles from "./Styling/app.module.scss";
 
 const App = () => {
   return (
     <div>
-      <div>Website here</div>
+      <Router>
+        {/* // ! Scroll to top will go here */}
+        {/* // ! Navbar might go here if it does not need to change on other pages */}
+
+        <main>
+          <Routes>
+            {/* // ! Routes will go here.. */}
+            {/* <Route path="/" exact component={Home} /> */}
+
+            {/*  // ! Replaces Redirect in react-router-dom 6 */}
+            {/* <Redirect to="/" exact /> */}
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </main>
+        {/* // ! Footer will go here.. */}
+      </Router>
     </div>
   );
 };
