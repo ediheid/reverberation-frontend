cae8a497ac9a284ab87e5a5c685315803718d7ad
Edith-Heidmann <eheidmann@gmail.com>
2022-03-10 21:01:15 +0100

    Imported framer motion and added animation to nav links on open of hamburger menu

--- src/Components/Navbar/MobileNavLinks.js
+++ src/Components/Navbar/MobileNavLinks.js
@@ -1,22 +1,73 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 // ? Styles
 import styles from "../Navbar/navbar.module.scss";
 
 const MobileNavLinks = () => {
+  const animateFrom = { opacity: 0, y: -40 };
+  const animateTo = { opacity: 1, y: 0 };
+
   return (
     <>
       <div className={styles["mobile-nav-dropdown-container"]}>
-        <ul className={styles["mobile-nav-list-items"]}>
-          <li>About</li>
-          <li>Line-up</li>
-          <li>Schedule</li>
-          <li>Past Events</li>
-          <li>Tickets</li>
-          <li>Contact</li>
+        <motion.ul
+          className={styles["mobile-nav-list-items"]}
+          initial={animateFrom}
+          animate={animateTo}
+          transition={{ delay: 0.05 }}
+        >
+          <motion.li
+            initial={animateFrom}
+            animate={animateTo}
+            transition={{ delay: 0.08 }}
+          >
+            About
+          </motion.li>
+          <motion.li
+            initial={animateFrom}
+            animate={animateTo}
+            transition={{ delay: 0.11 }}
+          >
+            Line-up
+          </motion.li>
+          <motion.li
+            initial={animateFrom}
+            animate={animateTo}
+            transition={{ delay: 0.14 }}
+          >
+            Schedule
+          </motion.li>
+          <motion.li
+            initial={animateFrom}
+            animate={animateTo}
+            transition={{ delay: 0.17 }}
+          >
+            Past Events
+          </motion.li>
+          <motion.li
+            initial={animateFrom}
+            animate={animateTo}
+            transition={{ delay: 0.2 }}
+          >
+            Tickets
+          </motion.li>
+          <motion.li
+            initial={animateFrom}
+            animate={animateTo}
+            transition={{ delay: 0.23 }}
+          >
+            Contact
+          </motion.li>
 
-          <li>Home</li>
-        </ul>
+          <motion.li
+            initial={animateFrom}
+            animate={animateTo}
+            transition={{ delay: 0.26 }}
+          >
+            Home
+          </motion.li>
+        </motion.ul>
       </div>
     </>
   );
