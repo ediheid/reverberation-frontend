a7784a7e436ccfe1f53f4aadf0f41720b40a19a3
Edith-Heidmann <eheidmann@gmail.com>
2022-05-23 18:31:54 +0200

    setup basic layout for section

--- src/Views-Sections/Home/Line-Up/LineUp.js
+++ src/Views-Sections/Home/Line-Up/LineUp.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import styles from "../Line-Up/line-up.module.scss";
+
 const LineUp = () => {
-  return <div>Line-Up section will go here</div>;
+  return (
+    <div className={styles["line-up-container"]}>
+      Line-Up section will go here
+    </div>
+  );
 };
 
 export default LineUp;
