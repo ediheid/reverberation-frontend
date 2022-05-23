8abf51e208ce7b9be084460aba73f75ab8d5e2c2
Edith-Heidmann <eheidmann@gmail.com>
2022-05-23 15:28:48 +0200

    Updated Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -12,6 +12,42 @@ const Footer = () => {
   return (
     <>
       <div className={styles["footer-container"]}>
+        <button className={styles["button-container"]}>
+          <Link
+            className={styles["button-a-link"]}
+            smooth
+            to="/contact"
+            // href="https://reverberation.loveyourartist.store/de/"
+            // target="_blank"
+            // rel="noopener noreferrer"
+            alt="Contact section"
+          >
+            Contact
+          </Link>
+        </button>
+
+        {/* Copyright and Impressum */}
+        <span className={styles["copyright-span"]}>
+          &copy; Dresden Psych Family e.V. {currentYear} | {/* // ! */}
+          {/* // Todo: Add impressum page link */}{" "}
+          <Link to="/impressum" className={styles["impressum-link"]}>
+            <b>IMPRESSUM</b>{" "}
+          </Link>
+        </span>
+
+        {/* Built by */}
+        <span className={styles["built-by"]}>
+          Website designed and developed by <br />
+          <a
+            href="https://www.edithsdev.com/"
+            target="_blank"
+            rel="noreferrer noopener"
+            alt="Link to developer site"
+          >
+            Edith Heidmann
+          </a>
+        </span>
+
         {/* Social media links */}
         <div className={styles["social-media-links-container"]}>
           <a
@@ -33,34 +69,6 @@ const Footer = () => {
             <FiInstagram className={styles["social-media-links"]} />
           </a>
         </div>
-
-        {/* Copyright and Impressum */}
-        <span className={styles["copyright-span"]}>
-          &copy; Dresden Psych Family e.V. {currentYear} | {/* // ! */}
-          {/* // Todo: Add impressum page link */}{" "}
-          <Link to="/impressum" className={styles["impressum-link"]}>
-            <b>IMPRESSUM</b>{" "}
-          </Link>
-        </span>
-
-        {/* Built by */}
-        <span className={styles["built-by"]}>
-          Designed and developed by <br />
-          <a
-            href="https://www.edithsdev.com/"
-            target="_blank"
-            rel="noreferrer noopener"
-            alt="Link to developer site"
-          >
-            Edith Heidmann
-          </a>
-        </span>
-
-        {/* // ! */}
-        {/* // Todo: Add Social media links */}
-
-        {/* // ! */}
-        {/* // Todo: Impressum Link will go here - maybe set it that it pops up like in FreshBnb website */}
       </div>
     </>
   );
