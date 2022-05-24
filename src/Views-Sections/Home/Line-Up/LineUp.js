617bddb2859e70669e94bc571f383c32ae1c26bf
Edith-Heidmann <eheidmann@gmail.com>
2022-05-24 13:52:18 +0200

    Added psych font

--- src/Views-Sections/Home/Line-Up/LineUp.js
+++ src/Views-Sections/Home/Line-Up/LineUp.js
@@ -4,8 +4,8 @@ import styles from "../Line-Up/line-up.module.scss";
 
 const LineUp = () => {
   return (
-    <div className={styles["line-up-container"]}>
-      Line-Up section will go here
+    <div className={styles["line-up-container"]} id="line-up">
+      <h2 className={styles["section-heading"]}>To be announced</h2>
     </div>
   );
 };
