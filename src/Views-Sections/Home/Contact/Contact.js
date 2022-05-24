296b652e77d39f27a0609e09793d96a100a7b3df
Edith-Heidmann <eheidmann@gmail.com>
2022-05-24 17:13:14 +0200

    Updated contact section

--- src/Views-Sections/Home/Contact/Contact.js
+++ src/Views-Sections/Home/Contact/Contact.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import styles from "../Contact/contact.module.scss";
+
 const Contact = () => {
-  return <div id="contact">Contact Section will go here</div>;
+  return (
+    <div className={styles["contact-section-container"]} id="contact">
+      <h2 className={styles["contact-heading"]}>Contact</h2>
+    </div>
+  );
 };
 
 export default Contact;
