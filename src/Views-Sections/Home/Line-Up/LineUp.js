b9377bc81dd375762972453e6ebb45c6f09302a3
Edith-Heidmann <eheidmann@gmail.com>
2022-05-24 14:45:54 +0200

    Updated line-up section

--- src/Views-Sections/Home/Line-Up/LineUp.js
+++ src/Views-Sections/Home/Line-Up/LineUp.js
@@ -7,13 +7,15 @@ const LineUp = () => {
     <div className={styles["line-up-container"]} id="line-up">
       <h2 className={styles["section-heading"]}>Line-up</h2>
 
+      <h2 className={styles["section-heading"]}>To be Announced</h2>
+
       <div className={styles["bands-container"]}>
         {/* // ! Will make an array of bands and info and then map below - this is just for testing purposes */}
+        {/* <div className={styles["band-box"]}></div>
         <div className={styles["band-box"]}></div>
         <div className={styles["band-box"]}></div>
         <div className={styles["band-box"]}></div>
-        <div className={styles["band-box"]}></div>
-        <div className={styles["band-box"]}></div>
+        <div className={styles["band-box"]}></div> */}
       </div>
     </div>
   );
