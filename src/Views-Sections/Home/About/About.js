50051295fc4d393afd3ebb601f93bfc263c78b65
Edith-Heidmann <eheidmann@gmail.com>
2022-05-24 14:54:43 +0200

    Updated about section

--- src/Views-Sections/Home/About/About.js
+++ src/Views-Sections/Home/About/About.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import styles from "../About/about.module.scss";
+
 const About = () => {
-  return <div>About section will go here</div>;
+  return (
+    <div className={styles["about-section-container"]} id="about">
+      <div className={styles["bg-img-container"]}>
+        <div className={styles["content-container"]}>
+          <h2 className={styles["about-heading"]}>
+            About section will go here
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
