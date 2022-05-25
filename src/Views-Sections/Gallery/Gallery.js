9e8d432100ee3827c3748a8316740f6376477438
Edith-Heidmann <eheidmann@gmail.com>
2022-05-25 16:24:10 +0200

    Setting up conditional rendering of gallery

--- src/Views-Sections/Gallery/Gallery.js
+++ src/Views-Sections/Gallery/Gallery.js
@@ -1,166 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "../Gallery/gallery.module.scss";
 
-import img1 from "../Gallery/Static/Day1-2.png";
-import img2 from "../Gallery/Static/Day1-3.png";
-import img3 from "../Gallery/Static/Day1-4.png";
-import img4 from "../Gallery/Static/Day1-8.png";
-import img5 from "../Gallery/Static/Day1-9.png";
-import img6 from "../Gallery/Static/Day1-10.png";
-import img7 from "../Gallery/Static/Day1-11.png";
-import img8 from "../Gallery/Static/Day1-12.png";
-import img9 from "../Gallery/Static/Day1-13.png";
-import img10 from "../Gallery/Static/Day1-14.png";
-import img11 from "../Gallery/Static/Day1-23.png";
-import img12 from "../Gallery/Static/Day1-26.png";
-import img13 from "../Gallery/Static/Day1-29.png";
-import img14 from "../Gallery/Static/Day1-30.png";
-import img15 from "../Gallery/Static/Day1-37.png";
-import img16 from "../Gallery/Static/Day1-38.png";
-import img17 from "../Gallery/Static/Day1-39.png";
-import img18 from "../Gallery/Static/Day1-46.png";
-import img19 from "../Gallery/Static/Day1-47.png";
-import img20 from "../Gallery/Static/Day1-48.png";
-import img21 from "../Gallery/Static/Day1-49.png";
-import img22 from "../Gallery/Static/Day1-50.png";
-import img23 from "../Gallery/Static/Day1-53.png";
-import img24 from "../Gallery/Static/Day1-54.png";
-import img25 from "../Gallery/Static/Day1-55.png";
-import img26 from "../Gallery/Static/Day1-56.png";
-import img27 from "../Gallery/Static/Day1-58.png";
-import img28 from "../Gallery/Static/Day1-61.png";
+import Gallery2021Day1 from "./Gallery2021Day1";
 
-const photosDay1 = [
-  {
-    img: img1,
-    alt: "Insert image description here",
-  },
-  {
-    img: img2,
-    alt: "Insert image description here",
-  },
-  {
-    img: img3,
-    alt: "Insert image description here",
-  },
-  {
-    img: img4,
-    alt: "Insert image description here",
-  },
-  {
-    img: img5,
-    alt: "Insert image description here",
-  },
-  {
-    img: img6,
-    alt: "Insert image description here",
-  },
-  {
-    img: img7,
-    alt: "Insert image description here",
-  },
-  {
-    img: img8,
-    alt: "Insert image description here",
-  },
-  {
-    img: img9,
-    alt: "Insert image description here",
-  },
-  {
-    img: img10,
-    alt: "Insert image description here",
-  },
-  {
-    img: img11,
-    alt: "Insert image description here",
-  },
-  {
-    img: img12,
-    alt: "Insert image description here",
-  },
-  {
-    img: img13,
-    alt: "Insert image description here",
-  },
-  {
-    img: img14,
-    alt: "Insert image description here",
-  },
-  {
-    img: img15,
-    alt: "Insert image description here",
-  },
-  {
-    img: img16,
-    alt: "Insert image description here",
-  },
-  {
-    img: img17,
-    alt: "Insert image description here",
-  },
-  {
-    img: img18,
-    alt: "Insert image description here",
-  },
-  {
-    img: img19,
-    alt: "Insert image description here",
-  },
-  {
-    img: img20,
-    alt: "Insert image description here",
-  },
-  {
-    img: img21,
-    alt: "Insert image description here",
-  },
-  {
-    img: img22,
-    alt: "Insert image description here",
-  },
-  {
-    img: img23,
-    alt: "Insert image description here",
-  },
-  {
-    img: img24,
-    alt: "Insert image description here",
-  },
-  {
-    img: img25,
-    alt: "Insert image description here",
-  },
-  {
-    img: img26,
-    alt: "Insert image description here",
-  },
-  {
-    img: img27,
-    alt: "Insert image description here",
-  },
-  {
-    img: img28,
-    alt: "Insert image description here",
-  },
-];
+import img29 from "../Gallery/Static/Day2-1.png";
+import img30 from "../Gallery/Static/Day2-2.png";
+import img31 from "../Gallery/Static/Day2-5.png";
+import img32 from "../Gallery/Static/Day2-6.png";
+import img33 from "../Gallery/Static/Day2-7.png";
+import img34 from "../Gallery/Static/Day2-8.png";
+import img35 from "../Gallery/Static/Day2-11.png";
+import img36 from "../Gallery/Static/Day2-17.png";
+import img37 from "../Gallery/Static/Day2-18.png";
+import img38 from "../Gallery/Static/Day2-19.png";
+import img39 from "../Gallery/Static/Day2-20.png";
+import img40 from "../Gallery/Static/Day2-24.png";
+import img41 from "../Gallery/Static/Day2-28.png";
+import img42 from "../Gallery/Static/Day2-31.png";
+import img43 from "../Gallery/Static/Day2-32.png";
+import img44 from "../Gallery/Static/Day2-33.png";
+import img45 from "../Gallery/Static/Day2-36.png";
+import img46 from "../Gallery/Static/Day2-39.png";
+import img47 from "../Gallery/Static/Day2-40.png";
+import img48 from "../Gallery/Static/Day2-41.png";
+import img49 from "../Gallery/Static/Day2-43.png";
+import img50 from "../Gallery/Static/Day2-44.png";
+import img51 from "../Gallery/Static/Day2-45.png";
+import img52 from "../Gallery/Static/Day2-46.png";
+import img53 from "../Gallery/Static/Day2-47.png";
+import img54 from "../Gallery/Static/Day2-48.png";
+import img55 from "../Gallery/Static/Day2-49.png";
+import img56 from "../Gallery/Static/Day2-55.png";
+import img57 from "../Gallery/Static/Day2-56.png";
+import img58 from "../Gallery/Static/Day2-58.png";
+import img59 from "../Gallery/Static/Day2-62.png";
+import img60 from "../Gallery/Static/Day2-64.png";
+import img61 from "../Gallery/Static/Day2-66.png";
+import img62 from "../Gallery/Static/Day2-67.png";
+import img63 from "../Gallery/Static/Day2-68.png";
+import img64 from "../Gallery/Static/Day2-69.png";
+import img65 from "../Gallery/Static/Day2-74.png";
+import img66 from "../Gallery/Static/Day2-76.png";
+import img67 from "../Gallery/Static/Day2-80.png";
+import img68 from "../Gallery/Static/Day2-81.png";
+import img69 from "../Gallery/Static/Day2-86.png";
+import img70 from "../Gallery/Static/Day2-87.png";
+import img71 from "../Gallery/Static/Day2-88.png";
+import img72 from "../Gallery/Static/Day2-89.png";
+import img73 from "../Gallery/Static/Day2-90.png";
+import img74 from "../Gallery/Static/Day2-92.png";
+import img75 from "../Gallery/Static/Day2-93.png";
+import img76 from "../Gallery/Static/Day2-96.png";
+import img77 from "../Gallery/Static/Day2-98.png";
+import img78 from "../Gallery/Static/Day2-99.png";
+import img79 from "../Gallery/Static/Day2-100.png";
+import img80 from "../Gallery/Static/Day2-102.png";
+import img81 from "../Gallery/Static/Day2-105.png";
+import img82 from "../Gallery/Static/Day2-109.png";
+import img83 from "../Gallery/Static/Day2-110.png";
+import img84 from "../Gallery/Static/Day2-111.png";
+import img85 from "../Gallery/Static/Day2-114.png";
 
 const Gallery = () => {
+  const [page, setPage] = useState("");
+
+  const pages = [
+    {
+      id: "2021Day1",
+      component: <Gallery2021Day1 />,
+    },
+  ];
+
+  console.log("Test", pages[0].component);
+
+  let pageClick = () => {
+    setPage(pages[0].component);
+  };
+
   return (
     <div className={styles["gallery-page-container"]}>
       <div className={styles["content-container"]}>
         <div className={styles["sub-heading-container"]}>
-          <h3 className={styles["section-headings"]}>2021 · Day 1</h3>
+          <h3 className={styles["section-headings"]}>Gallery</h3>
+
+          <ul>
+            <h3>2021</h3>
+            <li>
+              <button onClick={pageClick}>Day 1</button>
+            </li>
+            <li>Day 2</li>
+            <li>Day 3</li>
+          </ul>
         </div>
 
-        {photosDay1.map((photo) => (
-          <img
-            className={styles["img-container"]}
-            src={photo.img}
-            alt={photo.img}
-          ></img>
-        ))}
+        <div>{page}</div>
+
+        {/* <Gallery2021Day1 /> */}
       </div>
     </div>
   );
---
+++ src/Views-Sections/Gallery/Gallery2021Day1.js
@@ -0,0 +1,167 @@
+import React from "react";
+
+import styles from "../Gallery/gallery.module.scss";
+
+import img1 from "../Gallery/Static/Day1-2.png";
+import img2 from "../Gallery/Static/Day1-3.png";
+import img3 from "../Gallery/Static/Day1-4.png";
+import img4 from "../Gallery/Static/Day1-8.png";
+import img5 from "../Gallery/Static/Day1-9.png";
+import img6 from "../Gallery/Static/Day1-10.png";
+import img7 from "../Gallery/Static/Day1-11.png";
+import img8 from "../Gallery/Static/Day1-12.png";
+import img9 from "../Gallery/Static/Day1-13.png";
+import img10 from "../Gallery/Static/Day1-14.png";
+import img11 from "../Gallery/Static/Day1-23.png";
+import img12 from "../Gallery/Static/Day1-26.png";
+import img13 from "../Gallery/Static/Day1-29.png";
+import img14 from "../Gallery/Static/Day1-30.png";
+import img15 from "../Gallery/Static/Day1-37.png";
+import img16 from "../Gallery/Static/Day1-38.png";
+import img17 from "../Gallery/Static/Day1-39.png";
+import img18 from "../Gallery/Static/Day1-46.png";
+import img19 from "../Gallery/Static/Day1-47.png";
+import img20 from "../Gallery/Static/Day1-48.png";
+import img21 from "../Gallery/Static/Day1-49.png";
+import img22 from "../Gallery/Static/Day1-50.png";
+import img23 from "../Gallery/Static/Day1-53.png";
+import img24 from "../Gallery/Static/Day1-54.png";
+import img25 from "../Gallery/Static/Day1-55.png";
+import img26 from "../Gallery/Static/Day1-56.png";
+import img27 from "../Gallery/Static/Day1-58.png";
+import img28 from "../Gallery/Static/Day1-61.png";
+
+const photosDay1 = [
+  {
+    img: img1,
+    alt: "Insert image description here",
+  },
+  {
+    img: img2,
+    alt: "Insert image description here",
+  },
+  {
+    img: img3,
+    alt: "Insert image description here",
+  },
+  {
+    img: img4,
+    alt: "Insert image description here",
+  },
+  {
+    img: img5,
+    alt: "Insert image description here",
+  },
+  {
+    img: img6,
+    alt: "Insert image description here",
+  },
+  {
+    img: img7,
+    alt: "Insert image description here",
+  },
+  {
+    img: img8,
+    alt: "Insert image description here",
+  },
+  {
+    img: img9,
+    alt: "Insert image description here",
+  },
+  {
+    img: img10,
+    alt: "Insert image description here",
+  },
+  {
+    img: img11,
+    alt: "Insert image description here",
+  },
+  {
+    img: img12,
+    alt: "Insert image description here",
+  },
+  {
+    img: img13,
+    alt: "Insert image description here",
+  },
+  {
+    img: img14,
+    alt: "Insert image description here",
+  },
+  {
+    img: img15,
+    alt: "Insert image description here",
+  },
+  {
+    img: img16,
+    alt: "Insert image description here",
+  },
+  {
+    img: img17,
+    alt: "Insert image description here",
+  },
+  {
+    img: img18,
+    alt: "Insert image description here",
+  },
+  {
+    img: img19,
+    alt: "Insert image description here",
+  },
+  {
+    img: img20,
+    alt: "Insert image description here",
+  },
+  {
+    img: img21,
+    alt: "Insert image description here",
+  },
+  {
+    img: img22,
+    alt: "Insert image description here",
+  },
+  {
+    img: img23,
+    alt: "Insert image description here",
+  },
+  {
+    img: img24,
+    alt: "Insert image description here",
+  },
+  {
+    img: img25,
+    alt: "Insert image description here",
+  },
+  {
+    img: img26,
+    alt: "Insert image description here",
+  },
+  {
+    img: img27,
+    alt: "Insert image description here",
+  },
+  {
+    img: img28,
+    alt: "Insert image description here",
+  },
+];
+
+const Gallery2021Day1 = () => {
+  return (
+    <div>
+      {/* <div className={styles["sub-heading-container"]}>
+        <h3 className={styles["section-headings"]}>2021 · Day 1</h3>
+      </div> */}
+
+      {photosDay1.map((photo) => (
+        <img
+          className={styles["img-container"]}
+          src={photo.img}
+          alt={photo.img}
+        ></img>
+      ))}
+    </div>
+  );
+};
+
+export default Gallery2021Day1;
