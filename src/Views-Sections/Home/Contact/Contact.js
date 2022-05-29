f745cbd6c4f69b221e5f47a722e21d1249b36022
Edith-Heidmann <eheidmann@gmail.com>
2022-05-29 18:58:56 +0200

    Updated contact

--- src/Views-Sections/Home/Contact/Contact.js
+++ src/Views-Sections/Home/Contact/Contact.js
@@ -10,54 +10,60 @@ const Contact = () => {
           <h2 className={styles["contact-heading"]}>Contact</h2>
 
           <div className={styles["contact-div"]}>
-            <p className={styles["contact-text"]}>Dresden Psych Family e.V.</p>
-            <p className={styles["contact-text"]}>Sternstraße 35</p>
-            <p className={styles["contact-text"]}>01139 Dresden</p>
+            <span
+              className={`${styles["contact-text"]} ${styles["address-heading"]}`}
+            >
+              Dresden Psych Family e.V.
+            </span>
+            <span
+              className={`${styles["contact-text"]} ${styles["address-heading"]}`}
+            >
+              Sternstraße 35
+            </span>
+            <span
+              className={`${styles["contact-text"]} ${styles["address-heading"]}`}
+            >
+              01139 Dresden
+            </span>
           </div>
 
           <div className={styles["contact-div"]}>
             <span className={styles["contact-text"]}>General Enquiries</span>
 
-            <span>
-              Julian Baunach
-              <a
-                className={styles["anchor-tags"]}
-                href="mailto:julian@reverberationfest.de"
-                alt="link to mail julian@reverberationfest.de"
-              >
-                julian@reverberationfest.de
-              </a>
-            </span>
+            <span className={styles["name-text"]}>Julian Baunach </span>
+            <a
+              className={styles["anchor-tags"]}
+              href="mailto:julian@reverberationfest.de"
+              alt="link to mail julian@reverberationfest.de"
+            >
+              julian@reverberationfest.de
+            </a>
           </div>
 
           <div className={styles["contact-div"]}>
             <span className={styles["contact-text"]}>Booking & Production</span>
 
-            <span>
-              Robin Heller
-              <a
-                className={styles["anchor-tags"]}
-                href="mailto:robin@reverberationfest.de"
-                alt="link to mail robin@reverberationfest.de"
-              >
-                robin@reverberationfest.de
-              </a>
-            </span>
+            <span className={styles["name-text"]}>Robin Heller</span>
+            <a
+              className={styles["anchor-tags"]}
+              href="mailto:robin@reverberationfest.de"
+              alt="link to mail robin@reverberationfest.de"
+            >
+              robin@reverberationfest.de
+            </a>
           </div>
 
           <div className={styles["contact-div"]}>
             <span className={styles["contact-text"]}>Booking & Production</span>
 
-            <span>
-              Hannes Fröhlich
-              <a
-                className={styles["anchor-tags"]}
-                href="mailto:hannes@reverberationfest.de"
-                alt="link to mail hannes@reverberationfest.de"
-              >
-                hannes@reverberationfest.de
-              </a>
-            </span>
+            <span className={styles["name-text"]}>Hannes Fröhlich </span>
+            <a
+              className={styles["anchor-tags"]}
+              href="mailto:hannes@reverberationfest.de"
+              alt="link to mail hannes@reverberationfest.de"
+            >
+              hannes@reverberationfest.de
+            </a>
           </div>
         </div>
       </div>
