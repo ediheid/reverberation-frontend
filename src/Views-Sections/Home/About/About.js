63c9022ef191445eb5fe77186dc35c03383fc5fa
Edith-Heidmann <eheidmann@gmail.com>
2022-05-29 16:58:28 +0200

    Added and styled about text

--- src/Views-Sections/Home/About/About.js
+++ src/Views-Sections/Home/About/About.js
@@ -7,9 +7,15 @@ const About = () => {
     <div className={styles["about-section-container"]} id="about">
       <div className={styles["bg-img-container"]}>
         <div className={styles["content-container"]}>
-          <h2 className={styles["about-heading"]}>
-            About section will go here
-          </h2>
+          <h2 className={styles["about-heading"]}>About</h2>
+          <p className={styles["about-text"]}>
+            Reverberation Fest is an annual music and arts festival taking place
+            over the course of three days in the city of Dresden in Germany.{" "}
+          </p>
+          <p className={styles["about-text"]}>
+            It connects music with theater, performance, dance and other forms
+            of art and was founded in 2014.
+          </p>
         </div>
       </div>
     </div>
