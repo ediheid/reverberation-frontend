c319ab8e27c0ef992c8237e02fdf17276cc1f623
ediheid <eheidmann@gmail.com>
2022-05-31 23:20:27 +0200

    Updated about text

--- src/Views-Sections/Home/About/About.js
+++ src/Views-Sections/Home/About/About.js
@@ -9,7 +9,7 @@ const About = () => {
         <div className={styles["content-container"]}>
           <h2 className={styles["about-heading"]}>About</h2>
           <p className={styles["about-text"]}>
-            Founded in 2014, Reverberation Fest is a 3 day music and arts festival held annually in Dresden, Germany.
+            Founded in 2014, Reverberation Fest is an annual 3 day music and arts festival held in Dresden, Germany.
           </p>
           <p className={styles["about-text"]}>
             Showcasing and connecting local and international music acts with theater, performance, dance and other forms
