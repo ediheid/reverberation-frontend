f76bae840b28fb0f875a5db19dcd8bcc59506ccd
Edith-Heidmann <eheidmann@gmail.com>
2022-06-06 13:36:52 +0200

    Updated array info

--- src/Views-Sections/Home/Line-Up/LineUp.js
+++ src/Views-Sections/Home/Line-Up/LineUp.js
@@ -5,93 +5,84 @@ import BandCards from "./BandCards";
 import styles from "../Line-Up/line-up.module.scss";
 
 const LineUp = () => {
-
   // Todo: Decide how to order - alphabetically? use sort method
-const bands = [
-  {
-bandName: "LoveNJoy",
-img: "",
-text: "lkjndlvjcnslda",
-alt: "",
+  const bands = [
+    {
+      bandName: "LoveNJoy",
+      country: "UKR",
+      img: "",
+      text: "lkjndlvjcnslda",
+      alt: "",
+    },
+    {
+      bandName: "The Big Idea",
+      country: "FR",
+      img: "",
+      text: "lkjndlvjcnslda",
+      alt: "",
+    },
+    {
+      bandName: "Mitsune",
+      country: "JP/AUS/DE",
+      img: "",
+      text: "lkjndlvjcnslda",
+      alt: "",
+    },
+    {
+      bandName: "Rolando Bruno",
+      country: "AR",
+      img: "",
+      text: "lkjndlvjcnslda",
+      alt: "",
+    },
+    {
+      bandName: "13yearcicada",
+      country: "DE",
+      img: "",
+      text: "lkjndlvjcnslda",
+      alt: "",
+    },
+    {
+      bandName: "Candy Flip DJ's",
+      country: "CZ",
+      img: "",
+      text: "lkjndlvjcnslda",
+      alt: "",
+    },
+    {
+      bandName: "Glue Trip",
+      country: "BR",
+      img: "",
+      text: "lkjndlvjcnslda",
+      alt: "",
+    },
+  ];
 
-  },
-  {
-    bandName: "The Big Idea",
-    img: "",
-    text: "lkjndlvjcnslda",
-    alt: "",
-    
-      },
-      {
-        bandName: "Mitsune",
-        img: "",
-        text: "lkjndlvjcnslda",
-        alt: "",
-        
-          },
-          {
-            bandName: "Rolando Bruno",
-            img: "",
-            text: "lkjndlvjcnslda",
-            alt: "",
-            
-              },
-              {
-                bandName: "13yearcicada",
-                img: "",
-                text: "lkjndlvjcnslda",
-                alt: "",
-                
-                  },
-                  {
-                    bandName: "Candy Flip DJ's",
-                    img: "",
-                    text: "lkjndlvjcnslda",
-                    alt: "",
-                    
-                      },
-                      {
-                        bandName: "Glue Trip",
-                        img: "",
-                        text: "lkjndlvjcnslda",
-                        alt: "",
-                        
-                          }
-                          
-]
+  // ! Sort bands alphabetically
+  bands.sort((a, b) => a.bandName.localeCompare(b.bandName));
 
-// ! Sort bands alphabetically
-bands.sort((a,b) => a.bandName.localeCompare(b.bandName))
-
-// console.log(bands)
-
+  // console.log(bands)
 
   return (
     <div className={styles["line-up-container"]} id="line-up">
-
       {/* // ? Saved for when it goes back to 'to be announced' */}
       {/* <h2 className={styles["section-heading"]}>Line-up</h2> */}
       {/* <h2 className={styles["section-heading"]}>To be Announced</h2> */}
 
       <div className={styles["bands-container"]}>
+        <BandCards />
 
-
-<BandCards/>
-
-      
-{/* Map sorted bands.. */}
+        {/* Map sorted bands.. */}
         {bands.map((band) => (
-
-  <div className={styles["band-box"]}>
-    
-    <img className={styles["band-img"]} src={band.img} alt={band.alt}></img>
-    <span className={styles["bandname"]}>{band.bandName}</span>
-    
-    </div>
-
-
+          <div className={styles["band-box"]}>
+            <img
+              className={styles["band-img"]}
+              src={band.img}
+              alt={band.alt}
+            ></img>
+            <span className={styles["bandname"]}>{band.bandName}</span>
+          </div>
         ))}
-      
       </div>
     </div>
   );
