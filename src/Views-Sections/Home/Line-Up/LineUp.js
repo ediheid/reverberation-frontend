15f02e611d20bf32735e80321973d5a7c243de04
Edith-Heidmann <eheidmann@gmail.com>
2022-07-11 13:58:21 +0200

    Updated lineup section

--- src/Views-Sections/Home/Line-Up/LineUp.js
+++ src/Views-Sections/Home/Line-Up/LineUp.js
@@ -11,57 +11,57 @@ import toolBandPhoto from "../Line-Up/Static/tool.jpeg";
 const LineUp = () => {
   const bands = [
     // ! TEST with Tool
-    {
-      bandName: "Tool",
-      country: "US",
-      img: toolBandPhoto,
-      text: "Tool is an American rock band from Los Angeles. Formed in 1990, the group's line-up includes vocalist Maynard James Keenan, guitarist Adam Jones and drummer Danny Carey. Justin Chancellor has been the band's bassist since 1995.",
-      alt: "",
-    },
-    // ! ======
+    // {
+    //   bandName: "Tool",
+    //   country: "US",
+    //   img: toolBandPhoto,
+    //   text: "Tool is an American rock band from Los Angeles. Formed in 1990, the group's line-up includes vocalist Maynard James Keenan, guitarist Adam Jones and drummer Danny Carey. Justin Chancellor has been the band's bassist since 1995.",
+    //   alt: "",
+    // },
+    // // ! ======
 
-    {
-      bandName: "LoveNJoy",
-      country: "UKR",
-      img: "",
-      text: "lkjndlvjcnslda",
-      alt: "",
-    },
-    {
-      bandName: "The Big Idea",
-      country: "FR",
-      img: "",
-      text: "lkjndlvjcnslda",
-      alt: "",
-    },
-    {
-      bandName: "Mitsune",
-      country: "JP/AUS/DE",
-      img: "",
-      text: "lkjndlvjcnslda",
-      alt: "",
-    },
-    {
-      bandName: "Rolando Bruno",
-      country: "ARG",
-      img: "",
-      text: "lkjndlvjcnslda",
-      alt: "",
-    },
-    {
-      bandName: "13yearcicada",
-      country: "DE",
-      img: "",
-      text: "lkjndlvjcnslda",
-      alt: "",
-    },
-    {
-      bandName: "Candy Flip DJ's",
-      country: "CZ",
-      img: "",
-      text: "lkjndlvjcnslda",
-      alt: "",
-    },
+    // {
+    //   bandName: "LoveNJoy",
+    //   country: "UKR",
+    //   img: "",
+    //   text: "lkjndlvjcnslda",
+    //   alt: "",
+    // },
+    // {
+    //   bandName: "The Big Idea",
+    //   country: "FR",
+    //   img: "",
+    //   text: "lkjndlvjcnslda",
+    //   alt: "",
+    // },
+    // {
+    //   bandName: "Mitsune",
+    //   country: "JP/AUS/DE",
+    //   img: "",
+    //   text: "lkjndlvjcnslda",
+    //   alt: "",
+    // },
+    // {
+    //   bandName: "Rolando Bruno",
+    //   country: "ARG",
+    //   img: "",
+    //   text: "lkjndlvjcnslda",
+    //   alt: "",
+    // },
+    // {
+    //   bandName: "13yearcicada",
+    //   country: "DE",
+    //   img: "",
+    //   text: "lkjndlvjcnslda",
+    //   alt: "",
+    // },
+    // {
+    //   bandName: "Candy Flip DJ's",
+    //   country: "CZ",
+    //   img: "",
+    //   text: "lkjndlvjcnslda",
+    //   alt: "",
+    // },
     {
       bandName: "Glue Trip",
       country: "BRA",
@@ -71,31 +71,31 @@ const LineUp = () => {
     },
   ];
 
-  const artists = [
-    {
-      artistName: "Magdalena Dzeko",
-      country: "DE",
-      img: "",
-      text: "lkjndlvjcnslda",
-      alt: "",
-    },
+  // const artists = [
+  //   {
+  //     artistName: "Magdalena Dzeko",
+  //     country: "DE",
+  //     img: "",
+  //     text: "lkjndlvjcnslda",
+  //     alt: "",
+  //   },
 
-    {
-      artistName: "Tereza Chudackova",
-      country: "CZ",
-      img: "",
-      text: "lkjndlvjcnslda",
-      alt: "",
-    },
+  //   {
+  //     artistName: "Tereza Chudackova",
+  //     country: "CZ",
+  //     img: "",
+  //     text: "lkjndlvjcnslda",
+  //     alt: "",
+  //   },
 
-    {
-      artistName: "Aneta Navratilova",
-      country: "CZ",
-      img: "",
-      text: "lkjndlvjcnslda",
-      alt: "",
-    },
-  ];
+  //   {
+  //     artistName: "Aneta Navratilova",
+  //     country: "CZ",
+  //     img: "",
+  //     text: "lkjndlvjcnslda",
+  //     alt: "",
+  //   },
+  // ];
 
   // ! Sort bands alphabetically
   const sortBandsAlphabetically = () => {
@@ -104,12 +104,12 @@ const LineUp = () => {
 
   sortBandsAlphabetically();
 
-  // ! Sort bands alphabetically
-  const sortArtistsAlphabetically = () => {
-    return artists.sort((a, b) => a.artistName.localeCompare(b.artistName));
-  };
+  // ! Sort artists alphabetically
+  // const sortArtistsAlphabetically = () => {
+  //   return artists.sort((a, b) => a.artistName.localeCompare(b.artistName));
+  // };
 
-  sortArtistsAlphabetically();
+  // sortArtistsAlphabetically();
 
   return (
     <div className={styles["line-up-container"]} id="line-up">
@@ -118,7 +118,7 @@ const LineUp = () => {
       {/* <h2 className={styles["section-heading"]}>To be Announced</h2> */}
 
       {/* // ? Music */}
-      <h2 className={styles["announced-section-headings"]}>Music Acts</h2>
+      <h2 className={styles["announced-section-headings"]}>Music Line-up</h2>
       <div className={styles["bands-container"]}>
         {/* Map sorted bands.. */}
         {bands.map((band, index) => (
@@ -127,15 +127,19 @@ const LineUp = () => {
       </div>
 
       {/* // ? Artists and Performance */}
-      <h2 className={styles["announced-section-headings"]}>
+      {/* <h2 className={styles["announced-section-headings"]}>
         Artists and Performance
       </h2>
       <div className={styles["bands-container"]}>
-        {/* Map sorted Artists.. */}
+       
         {artists.map((artist, index) => (
           <ArtistCards key={index} artist={artist} />
         ))}
-      </div>
+      </div> */}
+
+      <h2 className={styles["announced-section-headings"]}>
+        Stay tuned, more to be announced soon!
+      </h2>
     </div>
   );
 };
