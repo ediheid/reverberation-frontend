14bdb260eb50ed5afb4cd950b3d348ad54869d35
Edith-Heidmann <eheidmann@gmail.com>
2022-07-12 18:09:30 +0200

    Breaking down componenets and bands array

---
+++ src/Views-Sections/Home/Line-Up/Bands.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+// Images
+import glueTrip from "./Static/glue-trip.png";
+
+export const bands = [
+  // {
+  //   bandName: "LoveNJoy",
+  //   country: "UKR",
+  //   img: "",
+  //   text: "lkjndlvjcnslda",
+  //   alt: "",
+  // },
+  // {
+  //   bandName: "The Big Idea",
+  //   country: "FR",
+  //   img: "",
+  //   text: "lkjndlvjcnslda",
+  //   alt: "",
+  // },
+  // {
+  //   bandName: "Mitsune",
+  //   country: "JP/AUS/DE",
+  //   img: "",
+  //   text: "lkjndlvjcnslda",
+  //   alt: "",
+  // },
+  // {
+  //   bandName: "Rolando Bruno",
+  //   country: "ARG",
+  //   img: "",
+  //   text: "lkjndlvjcnslda",
+  //   alt: "",
+  // },
+  // {
+  //   bandName: "13yearcicada",
+  //   country: "DE",
+  //   img: "",
+  //   text: "lkjndlvjcnslda",
+  //   alt: "",
+  // },
+  // {
+  //   bandName: "Candy Flip DJ's",
+  //   country: "CZ",
+  //   img: "",
+  //   text: "lkjndlvjcnslda",
+  //   alt: "",
+  // },
+  {
+    bandName: "Glue Trip",
+    country: "BRA",
+    img: glueTrip,
+    text: "Glue Trip is a brazilian psychedelic pop band from João Pessoa. They are currently fighting the evil forces of caretice.",
+    alt: "",
+  },
+];
--- src/Views-Sections/Home/Line-Up/LineUp.js
+++ src/Views-Sections/Home/Line-Up/LineUp.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { bands } from "./Bands";
+
 import BandCards from "../Line-Up/BandCards";
 import ArtistCards from "../Line-Up/ArtistCards";
 
@@ -9,57 +11,57 @@ import styles from "../Line-Up/line-up.module.scss";
 import glueTrip from "./Static/glue-trip.png";
 
 const LineUp = () => {
-  const bands = [
-    // {
-    //   bandName: "LoveNJoy",
-    //   country: "UKR",
-    //   img: "",
-    //   text: "lkjndlvjcnslda",
-    //   alt: "",
-    // },
-    // {
-    //   bandName: "The Big Idea",
-    //   country: "FR",
-    //   img: "",
-    //   text: "lkjndlvjcnslda",
-    //   alt: "",
-    // },
-    // {
-    //   bandName: "Mitsune",
-    //   country: "JP/AUS/DE",
-    //   img: "",
-    //   text: "lkjndlvjcnslda",
-    //   alt: "",
-    // },
-    // {
-    //   bandName: "Rolando Bruno",
-    //   country: "ARG",
-    //   img: "",
-    //   text: "lkjndlvjcnslda",
-    //   alt: "",
-    // },
-    // {
-    //   bandName: "13yearcicada",
-    //   country: "DE",
-    //   img: "",
-    //   text: "lkjndlvjcnslda",
-    //   alt: "",
-    // },
-    // {
-    //   bandName: "Candy Flip DJ's",
-    //   country: "CZ",
-    //   img: "",
-    //   text: "lkjndlvjcnslda",
-    //   alt: "",
-    // },
-    {
-      bandName: "Glue Trip",
-      country: "BRA",
-      img: glueTrip,
-      text: "Glue Trip is a brazilian psychedelic pop band from João Pessoa. They are currently fighting the evil forces of caretice.",
-      alt: "",
-    },
-  ];
+  // const bands = [
+  //   // {
+  //   //   bandName: "LoveNJoy",
+  //   //   country: "UKR",
+  //   //   img: "",
+  //   //   text: "lkjndlvjcnslda",
+  //   //   alt: "",
+  //   // },
+  //   // {
+  //   //   bandName: "The Big Idea",
+  //   //   country: "FR",
+  //   //   img: "",
+  //   //   text: "lkjndlvjcnslda",
+  //   //   alt: "",
+  //   // },
+  //   // {
+  //   //   bandName: "Mitsune",
+  //   //   country: "JP/AUS/DE",
+  //   //   img: "",
+  //   //   text: "lkjndlvjcnslda",
+  //   //   alt: "",
+  //   // },
+  //   // {
+  //   //   bandName: "Rolando Bruno",
+  //   //   country: "ARG",
+  //   //   img: "",
+  //   //   text: "lkjndlvjcnslda",
+  //   //   alt: "",
+  //   // },
+  //   // {
+  //   //   bandName: "13yearcicada",
+  //   //   country: "DE",
+  //   //   img: "",
+  //   //   text: "lkjndlvjcnslda",
+  //   //   alt: "",
+  //   // },
+  //   // {
+  //   //   bandName: "Candy Flip DJ's",
+  //   //   country: "CZ",
+  //   //   img: "",
+  //   //   text: "lkjndlvjcnslda",
+  //   //   alt: "",
+  //   // },
+  //   {
+  //     bandName: "Glue Trip",
+  //     country: "BRA",
+  //     img: glueTrip,
+  //     text: "Glue Trip is a brazilian psychedelic pop band from João Pessoa. They are currently fighting the evil forces of caretice.",
+  //     alt: "",
+  //   },
+  // ];
 
   // const artists = [
   //   {
