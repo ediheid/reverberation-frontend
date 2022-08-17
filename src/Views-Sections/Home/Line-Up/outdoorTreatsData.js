428604d4cde0c51f314b9514c69ad4a693b70d97
Edith-Heidmann <eheidmann@gmail.com>
2022-08-17 15:02:41 +0200

    Added Mission Lifeline data to outdoor cards

--- src/Views-Sections/Home/Line-Up/outdoorTreatsData.js
+++ src/Views-Sections/Home/Line-Up/outdoorTreatsData.js
@@ -16,17 +16,17 @@ export const outdoor = [
     outdoorName: "Lose Dresden",
     country: "DE",
     img: loseDresden,
-    text: "Lose Dresden is a zero waste/package-free store that has been supporting us right since we started 8 years ago. This year Berit, the founder and owner of Lose will also have a stand on Saturday at Chemiefabrik and be there to answer all your questions regarding waste reduction, sustainable nutrition and how it is still possible to save the planet 🌍!",
+    text: "Lose Dresden is a zero waste/package-free store that has been supporting us right since we started 8 years ago. This year Berit, the founder and owner of Lose will also have a stand on Saturday at Chemiefabrik and be there to answer all your questions regarding waste reduction, sustainable nutrition and how it is still possible to save the planet 🌍 !",
     link: "https://www.instagram.com/losedresden/",
     alt: "Owner, Berit Heller standing in her shop with package free products.",
   },
 
   {
-    outdoorName: "Lose Dresden",
+    outdoorName: "Mission Lifeline",
     country: "DE",
-    img: loseDresden,
-    text: "Together with hundreds of volunteers, Mission Lifeline works exclusively on a donor-funded basis. Without this support, their work would not be possible. Whether at sea or on land, in the Mediterranean, the Atlantic, Afghanistan or Ukraine - Because every life counts. Mission Lifeline will be at Chemifabrik to inform you about their activities, and how you can get involved and support.",
-    link: "https://www.instagram.com/losedresden/",
-    alt: "Owner, Berit Heller standing in her shop with package free products.",
+    img: missionLifeline,
+    text: "Together with hundreds of volunteers, Mission Lifeline works exclusively on a donor-funded basis. Without this support, their work would not be possible. Whether at sea or on land, in the Mediterranean, the Atlantic, Afghanistan or Ukraine - Because every life counts. Mission Lifeline will be at Chemifabrik to inform you about their activities and how you can get involved and support.",
+    link: "https://mission-lifeline.de/es-ist-einfach-noch-nicht-vorbei/",
+    alt: `Mission Lifeline logo and text "Mission Lifeline - Search and Rescue" in red on a white background.`,
   },
 ];
