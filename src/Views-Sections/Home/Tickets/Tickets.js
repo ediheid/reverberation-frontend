7228d666f5c6b36f137dbe65a098f80424622b4a
Edith-Heidmann <eheidmann@gmail.com>
2022-09-15 13:35:34 +0200

    Styled ticket prices info

--- src/Views-Sections/Home/Tickets/Tickets.js
+++ src/Views-Sections/Home/Tickets/Tickets.js
@@ -7,7 +7,7 @@ const Tickets = () => {
     <div className={styles["tickets-section-container"]} id="tickets">
       <div className={styles["bg-img-container"]}>
         <div className={styles["content-container"]}>
-          <h2 className={styles["tickets-heading"]}>Ticket Info</h2>
+          <h2 className={styles["tickets-heading"]}>Ticket Pricing Info</h2>
           {/* <a
             href="https://reverberation.loveyourartist.store/de/"
             target="_blank"
@@ -18,15 +18,63 @@ const Tickets = () => {
           </a> */}
           <section className={styles["all-days-container"]}>
             <div className={styles["day-container"]}>
-              <h3 className={styles["sub-headings"]}>Friday</h3>
+              {/* Friday */}
+              <h3 className={styles["sub-headings"]}>
+                Friday <span>(day pass)</span>
+              </h3>
+              <ul className={styles["list"]}>
+                <li className={styles["ticket-prices"]}>
+                  Pre-Sale: <span>16,50 EUR incl. fees</span>
+                </li>
+                <li className={styles["ticket-prices"]}>
+                  Box Office: <span>20,00 EUR</span>
+                </li>
+              </ul>
             </div>
 
+            {/*  Saturday */}
             <div className={styles["day-container"]}>
-              <h3 className={styles["sub-headings"]}>Saturday</h3>
+              <h3 className={styles["sub-headings"]}>
+                Saturday <span>(day pass)</span>
+              </h3>
+              <ul className={styles["list"]}>
+                <li className={styles["ticket-prices"]}>
+                  Pre-Sale: <span>27,50 EUR incl. fees</span>
+                </li>
+                <li className={styles["ticket-prices"]}>
+                  Box Office: <span>30,00 EUR</span>
+                </li>
+              </ul>
             </div>
 
+            {/*  Sunday */}
             <div className={styles["day-container"]}>
-              <h3 className={styles["sub-headings"]}>Sunday</h3>
+              <h3 className={styles["sub-headings"]}>
+                Sunday <span>(day pass)</span>
+              </h3>
+              <ul className={styles["list"]}>
+                <li className={styles["ticket-prices"]}>
+                  Pre-Sale: <span>16,50 EUR incl. fees</span>
+                </li>
+                <li className={styles["ticket-prices"]}>
+                  Box Office: <span>20,00 EUR</span>
+                </li>
+              </ul>
+            </div>
+
+            {/*  Festival Pass */}
+            <div className={styles["day-container"]}>
+              <h3 className={styles["sub-headings"]}>
+                Festival Pass <span>(3 days)</span>
+              </h3>
+              <ul className={styles["list"]}>
+                <li className={styles["ticket-prices"]}>
+                  Pre-Sale: <span>44,00 EUR incl. fees</span>
+                </li>
+                <li className={styles["ticket-prices"]}>
+                  Box Office: <span>45,00 EUR</span>
+                </li>
+              </ul>
             </div>
           </section>
         </div>
