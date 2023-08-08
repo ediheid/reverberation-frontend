81b23031ce607400c99538b2a62f1ade7b9ef1fd
Edith-Heidmann <eheidmann@gmail.com>
2023-08-08 16:53:06 +0200

    fix landing area

--- src/Views-Sections/Home/LandingArea/LandingArea.js
+++ src/Views-Sections/Home/LandingArea/LandingArea.js
@@ -1,3 +1,55 @@
+// import React from "react";
+// import { HashLink } from "react-router-hash-link";
+
+// import styles from "../LandingArea/landing-area.module.scss";
+
+// const LandingArea = () => {
+//   return (
+//     <div className={styles["landing-area-container"]} id="landing">
+//       <div className={styles["landing-area-extra-content"]}>
+//         <div className={styles["text-effect"]}>
+//           <h1
+//             className={`${styles["neon"]} ${styles["main-heading"]}`}
+//             data-text="REVERBERATION Festival"
+//           >
+//             REVERBERATION
+//             <br />
+//             Festival
+//           </h1>
+
+//           <div className={styles["gradient"]}></div>
+//           <div className={styles["spotlight"]}></div>
+//         </div>
+
+//         {/* <h2 className={styles["sub-headings"]}>Festival for Music & Arts</h2> */}
+
+//         <h2 className={styles["sub-headings"]}>
+//           Dresden · 16.09 - 18.09 · 2022
+//         </h2>
+
+//         {/* <h2 className={styles["sub-headings"]}>
+//           Day tickets and weekend passes available now!
+//         </h2> */}
+
+//         <HashLink smooth to="/#tickets">
+//           <button className={styles["ticket-info-button"]}>Ticket info</button>
+//         </HashLink>
+
+//         <a
+//           href="https://reverberation.loveyourartist.store/de/"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//           alt="Link to Ticket Shop"
+//         >
+//           <button className={styles["main-button"]}>Ticket Shop</button>
+//         </a>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default LandingArea;
+
 import React from "react";
 import { HashLink } from "react-router-hash-link";
 
@@ -11,6 +63,7 @@ const LandingArea = () => {
           <h1
             className={`${styles["neon"]} ${styles["main-heading"]}`}
             data-text="REVERBERATION Festival"
+            contenteditable
           >
             REVERBERATION
             <br />
@@ -23,16 +76,20 @@ const LandingArea = () => {
 
         {/* <h2 className={styles["sub-headings"]}>Festival for Music & Arts</h2> */}
 
-        <h2 className={styles["sub-headings"]}>
-          Dresden · 16.09 - 18.09 · 2022
-        </h2>
-
         {/* <h2 className={styles["sub-headings"]}>
-          Day tickets and weekend passes available now!
+          Dresden · 16.09 - 18.09 · 2022
         </h2> */}
 
-        <HashLink smooth to="/#tickets">
+        <h2 className={styles["sub-headings"]}>Presents:</h2>
+
+        <h2 className={styles["sub-headings"]}>A selection of Berlin shows</h2>
+
+        {/* <HashLink smooth to="/#tickets">
           <button className={styles["ticket-info-button"]}>Ticket info</button>
+        </HashLink> */}
+
+        <HashLink smooth to="/#upcoming">
+          <button className={styles["ticket-info-button"]}>More info</button>
         </HashLink>
 
         <a
@@ -41,7 +98,7 @@ const LandingArea = () => {
           rel="noopener noreferrer"
           alt="Link to Ticket Shop"
         >
-          <button className={styles["main-button"]}>Ticket Shop</button>
+          <button className={styles["main-button"]}>Tickets</button>
         </a>
       </div>
     </div>
